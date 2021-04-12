402ac032e954a8c301715afb448d981cb708fbfe
DaniVagyok <jojartd@gmail.com>
2021-04-12 16:32:06 +0200

    updated licenses and new activity

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.testproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_add_user:Button = findViewById(R.id.button_add_user)
+        button_add_user.setOnClickListener{
+            val intent = Intent(this, AddUserActivity::class.java)
+            startActivity(intent)
+
+        }
     }
+
+
 }
